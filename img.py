095113927f347b446736681de2ba22e5c9d9a238
Fredy Álvarez <fre.alvarezb@gmail.com>
2018-04-16 17:57:51 -0500

    demo and count mines

--- img.py
+++ img.py
@@ -1,4 +1,17 @@
 from random import shuffle, randrange
+
+def make_mazeNN(w = 3, h = 3):
+    vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
+    ver = [["@-"] * w + ['@'] for _ in range(h)] + [[]]
+    hor = [["@@"] * w + ['@'] for _ in range(h + 1)]
+ 
+    w = []
+    for (a, b) in zip(hor, ver):
+        w.append(''.join(a))
+        w.append(''.join(b))  
+       
+    return w
+ 
  
 def make_maze(w = 3, h = 3):
     vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
@@ -31,39 +44,48 @@ def make_maze(w = 3, h = 3):
        
     return s,w
  
+def mineCounter(maze):
+    aux=[]
+    for k in range(0,len(maze)):
+        s1=""
+        for l in range(0, len(maze[k])):
+            cont = 0
+            if maze[k][l] == "-":
+                if k-1>=0 and maze[k-1][l]=="@":
+                    cont+=1
+                if k+1<len(maze) and maze[k+1][l]=="@":
+                    cont+=1
+                if l-1>=0 and maze[k][l-1]=="@":
+                    cont+=1
+                if l+1<len(maze[k]) and maze[k][l+1]=="@":
+                    cont+=1
+                if k-1>=0 and l-1>=0 and maze[k-1][l-1]=="@":
+                    cont+=1
+                if k+1<len(maze) and l+1<len(maze[k]) and maze[k+1][l+1]=="@":
+                    cont+=1
+                if k-1>=0 and l+1<len(maze[k]) and maze[k-1][l+1]=="@":
+                    cont+=1
+                if k+1<len(maze) and l-1>=0 and maze[k+1][l-1]=="@":
+                    cont+=1
+                s1+= str(cont)
+            else:
+                s1+=str(maze[k][l])
+        aux.append(s1)
+    return aux
+
+
 if __name__ == '__main__':
+    maze3 = make_mazeNN();
+    for i in maze3:
+        print(i)
+
     maze,maze1 = make_maze()
     for i in maze:
         print(i) 
     for j in maze1:
-        print j 
+        print(j)
     maze2 = maze1[1:-2]
-    aux=[]
-    for k in range(0,len(maze2)):
-        
-        s1=""
-        for l in range(0, len(maze2[k])):
-            cont = 0
-            if maze2[k][l] == "-":
-                if k-1>=0 and maze2[k-1][l]=="@":
-                    cont+=1
-                if k+1<len(maze2) and maze2[k+1][l]=="@":
-                    cont+=1
-                if l-1>=0 and maze2[k][l-1]=="@":
-                    cont+=1
-                if l+1<len(maze2[k]) and maze2[k][l+1]=="@":
-                    cont+=1
-                if k-1>=0 and l-1>=0 and maze2[k-1][l-1]=="@":
-                    cont+=1
-                if k+1<len(maze2) and l+1<len(maze2[k]) and maze2[k+1][l+1]=="@":
-                    cont+=1
-                if k-1>=0 and l+1<len(maze2[k]) and maze2[k-1][l+1]=="@":
-                    cont+=1
-                if k+1<len(maze2) and l-1>=0 and maze2[k+1][l-1]=="@":
-                    cont+=1
-                s1+= str(cont)
-            else:
-                s1+=str(maze2[k][l])
-        aux.append(s1)
+
+    aux = mineCounter(maze2)
     for j in aux:
-        print j
+        print(j)
