ddd9afc56d991e419e5c1a1158208c730fd0add1
Fredy Álvarez <fre.alvarezb@gmail.com>
2018-04-28 20:11:52 -0500

    maze generator without external libraries

--- maze.py
+++ maze.py
@@ -1,7 +1,4 @@
-import numpy
-from numpy.random import random_integers as rand
-import matplotlib.pyplot as pyplot
-
+from random import randrange as rand
 def maze(width=81, height=51, complexity=.75, density=.75):
     # Only odd shapes
     shape = ((height // 2) * 2 + 1, (width // 2) * 2 + 1)
@@ -9,14 +6,16 @@ def maze(width=81, height=51, complexity=.75, density=.75):
     complexity = int(complexity * (5 * (shape[0] + shape[1])))
     density    = int(density * ((shape[0] // 2) * (shape[1] // 2)))
     # Build actual maze
-    Z = numpy.zeros(shape, dtype=bool)
+    Z = [[False] * shape[1] for i in range(shape[0])]
     # Fill borders
-    Z[0, :] = Z[-1, :] = 1
-    Z[:, 0] = Z[:, -1] = 1
+    Z[0] = Z[-1] = [True] * shape[1]
+    for i in range(len(Z)):
+        Z[i][0] = True
+        Z[i][-1] = True
     # Make aisles
     for i in range(density):
         x, y = rand(0, shape[1] // 2) * 2, rand(0, shape[0] // 2) * 2
-        Z[y, x] = 1
+        Z[y][x] = True
         for j in range(complexity):
             neighbours = []
             if x > 1:             neighbours.append((y, x - 2))
@@ -25,23 +24,18 @@ def maze(width=81, height=51, complexity=.75, density=.75):
             if y < shape[0] - 2:  neighbours.append((y + 2, x))
             if len(neighbours):
                 y_,x_ = neighbours[rand(0, len(neighbours) - 1)]
-                if Z[y_, x_] == 0:
-                    Z[y_, x_] = 1
-                    Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 1
+                if Z[y_][x_] == False:
+                    Z[y_][x_] = True
+                    Z[y_ + (y - y_) // 2][x_ + (x - x_) // 2] = True
                     x, y = x_, y_
     return Z
 
-mz = maze(7,7)
-#for i in mz:
-#	for j in i:
-#		if j :
-#			print ("█",end='')
-#		else:
-#			print (" ", end= '')
-#	print()
-
-
-pyplot.figure(figsize=(10, 5))
-pyplot.imshow(maze(100, 100), cmap=pyplot.cm.binary, interpolation='nearest')
-pyplot.xticks([]), pyplot.yticks([])
-pyplot.show()
+mz = maze(10,10)
+# density = 2
+for i in mz:
+    for j in i:
+        if j :
+            print("█",end='')
+        else:
+            print(" ", end= '')
+    print()
